98596d94db3be61f4e5809a77678a70e44cec076
p0lygun <solankivibhakar82@gmail.com>
2021-03-19 21:08:40 +0530

    added basic colors

--- wallpaper_engine/data/shared.py
+++ wallpaper_engine/data/shared.py
@@ -1,4 +1,13 @@
-from wallpaper_project.wallpaper_engine.libs.storage import Storage
+from ..libs.storage import Storage
 
 global storage
 storage = Storage()
+
+global colors
+colors = dict()
+
+colors['white'] = (255, 255, 255, 255)
+colors['black'] = (0, 0, 0, 255)
+colors['red'] = (255, 0, 0, 255)
+colors['green'] = (0, 255, 0, 255)
+colors['blue'] = [0, 0, 255, 255]
