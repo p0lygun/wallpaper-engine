f7e9aac4722c2340b1b9534ee9337cb7b6fef410
p0lygun <solankivibhakar82@gmail.com>
2021-09-01 03:33:29 +0530

    Set correct values for Kivy Config

--- wallpaper_engine/__main__.py
+++ wallpaper_engine/__main__.py
@@ -1,11 +1,24 @@
 from colorama import init
+import win32api
 
-from .libs.kivy_manager import WallpaperEngine
+from kivy.config import Config
+
 from .utils.logger import Logger
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     init()
+    Config.set("graphics", "borderless", "1")
+    Config.set("graphics", "resizable", "0")
+    Config.set("graphics", "width", f"{win32api.GetSystemMetrics(0)}")
+    Config.set("graphics", "height", f"{win32api.GetSystemMetrics(1)}")
+
     try:
+        from .libs.kivy_manager import WallpaperEngine
+
         WallpaperEngine().run()
     except KeyboardInterrupt:
+        from kivy.app import App
+
+        App.get_running_app().window_manager.reset_wallpaper()
         Logger.info("Exiting...")
