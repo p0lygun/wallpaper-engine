a2638da77fda9c0acbffecc354daf3f3b5cd32da
p0lygun <solankivibhakar82@gmail.com>
2021-09-03 02:20:16 +0530

    made logger user __name__

--- wallpaper_engine/libs/kivy_manager.py
+++ wallpaper_engine/libs/kivy_manager.py
@@ -6,28 +6,21 @@ from kivy.uix.label import Label
 from kivy.clock import Clock
 
 from ..utils.config import Config
-from ..utils.logger import Logger
+from ..utils.logger import LoggerClass
 from .window_manager import WindowManager
+
+Logger = LoggerClass(__name__)
 
 
 class WallpaperEngine(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Logger.set_level(logging.DEBUG)
-        self.we_config = Config(self.user_data_dir)
+        self.we_config = Config()
         self.window_manager = WindowManager()
         self.hwnd = None
 
     def on_start(self):
-        self.we_config.config.set("app", "first_run", False)
-        log_level = self.we_config.config.get("app", "log_level")
-        if log_level.isnumeric():
-            log_level = int(log_level)
-            if log_level in [0, 10, 20, 30, 40, 50]:
-                Logger.set_level(log_level)
-            else:
-                self.we_config.config.set("app", "log_level", 10)
-        self.we_config.write()
         self.title = (
             self.get_application_name() + "_" + self.we_config.config.get("app", "uuid")
         )
