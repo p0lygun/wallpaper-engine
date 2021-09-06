48a6422fdb0d386bf6fe95bf55fef5519d1379c8
p0lygun <solankivibhakar82@gmail.com>
2021-09-06 21:09:26 +0530

    added wallpaper base class

---
+++ wallpaper_engine/wallpapers/wallpaper_base.py
@@ -0,0 +1,12 @@
+from kivy.uix.floatlayout import FloatLayout
+
+
+class WallpaperBase(FloatLayout):
+    def __init__(self):
+        super(WallpaperBase, self).__init__()
+
+    def build(self):
+        pass
+
+    def animate(self):
+        pass
