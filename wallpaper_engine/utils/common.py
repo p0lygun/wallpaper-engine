c4ea4c256d268320d1d662755dc2b870c31af796
p0lygun <solankivibhakar82@gmail.com>
2021-09-08 22:41:42 +0530

    added available wallpapers

--- wallpaper_engine/utils/common.py
+++ wallpaper_engine/utils/common.py
@@ -9,3 +9,10 @@ commands = {
 }
 
 project_dir = pathlib.Path(__file__).parents[1]
+
+wallpaper_dir = project_dir / "wallpapers"
+valid_wallpapers = [
+    i.stem
+    for i in wallpaper_dir.glob("*.py")
+    if i.name not in ["wallpaper_base.py", "__init__.py"]
+]
