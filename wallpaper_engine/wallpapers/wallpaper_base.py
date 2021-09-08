b62be7309638ace7486db0919ca65945481f29aa
p0lygun <solankivibhakar82@gmail.com>
2021-09-08 10:55:25 +0530

    added play/pause

--- wallpaper_engine/wallpapers/wallpaper_base.py
+++ wallpaper_engine/wallpapers/wallpaper_base.py
@@ -7,9 +7,16 @@ class WallpaperBase(FloatLayout):
         super(WallpaperBase, self).__init__()
         self.app = App.get_running_app()
         self.container = None
+        self.animation_loop_clock = None
 
     def build(self):
         pass
 
     def animate(self):
+        pass
+
+    def play(self):
+        pass
+
+    def pause(self):
         pass
