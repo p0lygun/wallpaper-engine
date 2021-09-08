c7b18427ed9d0a2809bcf49eb88750b7ce57730c
p0lygun <solankivibhakar82@gmail.com>
2021-09-09 02:58:12 +0530

    added dynamic config loading

--- wallpaper_engine/wallpapers/wallpaper_base.py
+++ wallpaper_engine/wallpapers/wallpaper_base.py
@@ -1,4 +1,6 @@
+import json
 from kivy.uix.floatlayout import FloatLayout
+from kivy.utils import get_color_from_hex, get_hex_from_color
 from kivy.app import App
 
 
@@ -8,6 +10,7 @@ class WallpaperBase(FloatLayout):
         self.app = App.get_running_app()
         self.container = None
         self.animation_loop_clock = None
+        self.playing = False
 
     def build(self):
         pass
@@ -21,5 +24,55 @@ class WallpaperBase(FloatLayout):
     def pause(self):
         pass
 
-    def load_config(self):
-        pass
+    def load_config(self, json_dumps):
+        s_json = json.loads(json_dumps)
+        # set defaults
+        if not self.config.config.has_section("wallpaper"):
+            self.config.config.add_section("wallpaper")
+        for item in s_json:
+            if item["type"] != "title":
+                var_name = item["key"]
+                if "color" in var_name:
+                    self.config.config.setdefault(
+                        "wallpaper",
+                        var_name,
+                        get_hex_from_color(getattr(self, var_name)),
+                    )
+                else:
+                    self.config.config.setdefault(
+                        "wallpaper", var_name, getattr(self, var_name)
+                    )
+        self.config.reload()
+        self.config.write()
+        # read from config
+        for item in s_json:
+            if item["type"] != "title":
+                var_name = item["key"]
+                if "color" in var_name:
+                    setattr(
+                        self,
+                        var_name,
+                        get_color_from_hex(
+                            self.config.config.get("wallpaper", var_name)
+                        ),
+                    )
+                else:
+                    if item["type"] == "numeric":
+                        if item.get("is_int"):
+                            setattr(
+                                self,
+                                var_name,
+                                self.config.config.getint("wallpaper", var_name),
+                            )
+                        else:
+                            setattr(
+                                self,
+                                var_name,
+                                self.config.config.getfloat("wallpaper", var_name),
+                            )
+                    else:
+                        setattr(
+                            self,
+                            var_name,
+                            self.config.config.get("wallpaper", var_name),
+                        )
