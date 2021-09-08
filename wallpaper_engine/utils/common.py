c501c5e1735151857052a8e9201b77c85901294c
p0lygun <solankivibhakar82@gmail.com>
2021-09-09 04:52:43 +0530

    added build_settings_json to make data safe for kivy settings panel

--- wallpaper_engine/utils/common.py
+++ wallpaper_engine/utils/common.py
@@ -1,4 +1,5 @@
 import pathlib
+import json
 
 commands = {
     "VISIBILITY": 0,
@@ -16,3 +17,11 @@ valid_wallpapers = [
     for i in wallpaper_dir.glob("*.py")
     if i.name not in ["wallpaper_base.py", "__init__.py"]
 ]
+
+
+def build_settings_json(json_data: list) -> str:
+    tmp = json_data
+    for item in tmp:
+        if item.get("is_int"):
+            item.pop("is_int")
+    return json.dumps(tmp)
