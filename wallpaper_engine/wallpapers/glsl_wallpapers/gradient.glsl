ac6846d12259554abe3121aeb4099b75626fc73c
p0lygun <solankivibhakar82@gmail.com>
2022-02-21 20:56:05 +0530

    added a gradient wallpaper

---
+++ wallpaper_engine/wallpapers/glsl_wallpapers/gradient.glsl
@@ -0,0 +1,11 @@
+void main() {
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+
+    // Time varying pixel color
+    vec3 col = 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4));
+
+    // Output to screen
+    fragColor = vec4(col,1.0);
+
+}
